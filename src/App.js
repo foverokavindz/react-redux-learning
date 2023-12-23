ad4824b0db0bda032dba3d1e6f7b9bf8eb73e560
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2023-12-23 11:53:11 +0530

    remove previos basic codes to install redux tools

--- src/App.js
+++ src/App.js
@@ -1,52 +1,5 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { numberDecrement, numberIncrement } from './store/reducer/number';
-import { Home } from './pages/Home';
-
 const App = () => {
-  //store eken return ekak gnn puluwan function ekak widiyata
-  //useSelectore use krnnr store eka access krnn meka redux walin dena function ekak
-  const number = useSelector((store) => store.number);
-
-  // useDispatch hraha gnn reducer fuinction eken object eak pass krna eenaction eke ynne reducer eke action ekata
-  // etapsse eee actions switch case hrha filter krla thaama action kra krgnne
-  const dispatch = useDispatch();
-
-  return (
-    <div>
-      {number}
-      <br /> <br />
-      <div>
-        <button
-          onClick={() => {
-            /* meka hama thissema liynwt wada action ekk generate krna function ekak liynn puluwan */
-            /* 
-            
-            dispatch({
-              type: 'increment',
-              payload: 7,
-            });
-
-            */
-
-            // methanama payload eka denn puluwan
-            dispatch(numberIncrement(1));
-          }}
-        >
-          Increment
-        </button>
-        <button
-          onClick={() => {
-            dispatch(numberDecrement(1));
-          }}
-        >
-          Decrement
-        </button>
-      </div>
-      <br />
-      <div>{number}</div>
-      <Home />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,17 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-//import redux and store
-import { Provider } from 'react-redux';
-import store from './store/store';
-
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  </Provider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
 
 reportWebVitals();
