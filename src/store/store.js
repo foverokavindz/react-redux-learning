6403f811709863e20e5ce023e96c9200663b62f0
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2023-12-23 11:57:18 +0530

    remove previos basic codes to install redux tools - delete some files

--- src/store/store.js
+++
@@ -1,9 +0,0 @@
-import { createStore } from 'redux';
-
-import reducer from './reducer/number';
-// step - how change values in store
-
-//store eke statew eka wens krna function eka thma dispatch() kiyne
-const store = createStore(reducer);
-
-export default store;
