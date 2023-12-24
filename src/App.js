c6d9fd79fabcd5728082bec0a2e3b65cd8136dbe
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2023-12-24 11:18:37 +0530

    remove previos files and clearn project

--- src/App.js
+++ src/App.js
@@ -1,29 +1,5 @@
-import { useDispatch, useSelector } from 'react-redux';
-import { increment, decrement } from './store/reducers/numberSlice';
-import { laptopSliceSelector } from './store/reducers/laptopSlice';
-import LaptopComponent from './components/laptopComponent';
-
 const App = () => {
-  const number = useSelector((store) => store.numberSlice.number);
-  const number2 = useSelector((store) => store.number2Slice.number);
-  const dispatch = useDispatch();
-  return (
-    <div>
-      <h1> {number}</h1>
-
-      <button onClick={() => dispatch(increment(1))}>Increment</button>
-      <button onClick={() => dispatch(decrement(1))}>Decrement</button>
-
-      <hr />
-
-      {number2}
-
-      <br />
-      <br />
-
-      <LaptopComponent />
-    </div>
-  );
+  return <div></div>;
 };
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -5,15 +5,13 @@ import App from './App';
 import reportWebVitals from './reportWebVitals';
 
 // redux
-import { Provider } from 'react-redux';
-import store from './store/store';
+// import { Provider } from 'react-redux';
+// import store from './store/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
+    <App />
   </React.StrictMode>
 );
 
