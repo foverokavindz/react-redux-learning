949c42d06c00701bea504a062583c7ae34d434da
Kavinda Maduranga <kavindamadhuranga74@gmail.com>
2023-12-24 11:57:41 +0530

    Done Immer - producer function

--- src/App.js
+++ src/App.js
@@ -1,5 +1,34 @@
+import { useState } from 'react';
+import { produce } from 'immer';
+
+const obj = {
+  name: 'amila',
+  city: 'Tangalle',
+  position: 'Web Developer',
+  address: {
+    add1: 'nuwara',
+  },
+};
+
 const App = () => {
-  return <div></div>;
+  const [test, setTest] = useState({ ...obj });
+
+  const clickHandle = () => {
+    // react waladi producer function eka athulta prev state eka denna one na...specily react walt puluwan pre stat eka producer dunction ekt automaticaly arn new state ekk hdnna
+    // spread kr kr object copy arn wens krna ekt lesi widiyak thma Immer eken krnne
+    setTest(
+      produce((prevState) => {
+        prevState.address.add2 = 'Colombo';
+      })
+    );
+  };
+
+  return (
+    <div>
+      <button onClick={clickHandle}>Click me</button>
+      {console.log(test)}
+    </div>
+  );
 };
 
 export default App;
